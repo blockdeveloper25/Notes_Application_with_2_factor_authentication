7403f0a953bc2f7787fb6c80f79dbd17c4a4a14c
blockdeveloper25 <sujairblockdev@gmail.com>
2025-08-08 20:46:20 +0530

    cors and session connection

--- src/index.js
+++ src/index.js
@@ -9,8 +9,26 @@ dotenv.config();
 const app = express();
 
 //Middleware
+const corsOptions = {
+    origin: 'http://localhost:3001',
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(json({limit: '100mb'}));
 app.use(urlencoded({ extended: true, limit: '100mb' }));
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    cookie: {
+        maxAge:  60 * 60 * 1000, // session expires after 1 hour
+    },
+
+}))
+app.use(passport.initialize());
+app.use(passport.session())
+
 
 // Routes
 
