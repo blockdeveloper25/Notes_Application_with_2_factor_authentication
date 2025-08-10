c0add2d69117d552cf5c6410c0eaa8b8ecdd2e3f
blockdeveloper25 <sujairblockdev@gmail.com>
2025-08-10 12:19:53 +0530

    login form created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { router } from "./routes";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="bg-slate-900 h-screen">
+      <div className="flex justify-center items-center h-screen">
+        <RouterProvider router={router} />
+      </div>
+    </div>
   );
 }
 
